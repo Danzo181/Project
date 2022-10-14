a3b06f93603d581edffcdf69d2d600cd7584d451
danlam0809 <danlam0809@highgateschool.org.uk>
2022-10-14 16:01:06 +0100

    Changes fold function so that changing current player happens after current_raise_player to prevent an infinite loop. Also added setup function and removed that section from main and changed its name to single_round to be more adaptive.

--- Project.py
+++ Project.py
@@ -1,7 +1,7 @@
 import random
 
-class Card():
 
+class Card():
     suits = {"C": "Clubs", "S": "Spades", "H": "Hearts", "D": "Diamonds"}
     values = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "J": 11, "Q": 12, "K": 13,
               "A": 14}
@@ -17,7 +17,7 @@ class Card():
     def __repr__(self):
         return f"Card: {self.name} of {self.suits[self.suit]} "
 
-    def __eq__(self,other):
+    def __eq__(self, other):
         return self.value == other.value
 
     def __lt__(self, other):
@@ -26,6 +26,7 @@ class Card():
     def __gt__(self, other):
         return self.value > other.value
 
+
 class Deck():
 
     def __init__(self):
@@ -33,7 +34,7 @@ class Deck():
         self.table_hand = []
         for suit in Card.suits:
             for value in Card.values:
-                current_card= Card(value,suit)
+                current_card = Card(value, suit)
                 self.deck.append(current_card)
 
     def shuffle(self):
@@ -55,9 +56,10 @@ class Deck():
         self.table_hand.append(self.deck.pop(0))
         return self.table_hand
 
+
 class Table():
 
-    def __init__(self,hands_num,bank_amount=1500):
+    def __init__(self, hands_num, bank_amount=1500):
         self.deck = Deck()
         self.deck.shuffle()
         self.hands = []
@@ -66,13 +68,11 @@ class Table():
         self.player_keys = []
         self.bank_amount = bank_amount
 
-
         for i in range(hands_num):
-            hand =[]
+            hand = []
             for i in range(2):
                 hand.append(self.deck.deal_hand())
             self.hands.append(hand)
-
 
         for i in range(self.hands_num):
             p_name = input("enter player name: ")
@@ -80,12 +80,12 @@ class Table():
 
         self.player_keys = list(self.player_lst.keys())
 
-
     def show_hand(self, player_num):
         return self.hands[player_num]
 
+
 class Game():
-    def __init__(self,players = 5, rounds = 5,blinds = 20):
+    def __init__(self, players=5, rounds=5, blinds=20):
         self.table = Table(players)
         self.num_rounds = rounds
         self.blinds = blinds
@@ -99,10 +99,10 @@ class Game():
         self.current_play = 0
         self.current_raise_player = self.dealer + 1
 
-    def change_item(self,player):
+    def change_item(self, player):
         self.table.player_lst[player] = self.current_player_lst[player]
 
-    def money_exchange(self,amount,player_name):
+    def money_exchange(self, amount, player_name):
         self.current_player_lst[player_name] -= amount
         self.pot_amount += amount
 
@@ -111,14 +111,14 @@ class Game():
         self.current_raise = int(input("Enter amount to raise by: "))
         self.current_raise_player = player_num
 
-    def Fold(self,player_num):
+    def Fold(self, player_num):
         del self.current_player_lst[self.current_player_keys[player_num]]
         self.current_player_keys.pop(player_num)
         self.table.hands.pop(player_num)
-        self.current_player -= 1
 
         if self.current_player <= self.current_raise_player:
             self.current_raise_player -= 1
+        self.current_player -= 1
 
     def betting(self):
 
@@ -148,6 +148,7 @@ class Game():
         while self.current_player != self.current_raise_player or self.passes <= 0:
             print(self.current_player)
             print(self.current_raise_player)
+            print(self.passes)
             print(self.current_player_lst)
             print(self.table.show_hand(self.current_player))
             print(self.table.deck.table_hand)
@@ -183,11 +184,15 @@ class Game():
         self.passes = 0
 
 
-def main():
+def setup():
     num_players = int(input("players: "))
 
     current_game = Game(num_players)
 
+    return current_game
+
+
+def single_round(current_game):
     while len(current_game.current_player_lst) > 1:
 
         if current_game.current_play == 0:
@@ -227,11 +232,11 @@ def main():
     # if len(current_game.current_player_lst) <= 1:
     #     current_game.end_round()
 
-#Game(1)
-main()
 
+# Game(1)
+single_round(setup())
 
-#tab = Table(3)
-#card_KH = Card("K","H")
-#card_2S = Card("2","S")
-#deck = Deck
+# tab = Table(3)
+# card_KH = Card("K","H")
+# card_2S = Card("2","S")
+# deck = Deck
