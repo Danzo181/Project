5d6c69648eaa80d016d098a5468424dc356ccc17
dan <danlam0809@highgateschool.org.uk>
2023-01-04 14:54:39 +0000

    Changed num_players_setup to make it work and allows the player to enter all names without 8 different variables through recursion. Finished player_name_fn to properly push the inputted player names to the position on the screen. When implemented into the real code it will need to also add the player name to the dictionary of player names. Mostly finished add_chip_counts but need to change the variable of chip_count to properly display on each player.

--- Tkinter/Tkinter main.py
+++ Tkinter/Tkinter main.py
@@ -11,67 +11,90 @@ class setup_frame:
         self.setup_frame.grid(row=0, column=0, sticky="news")
 
         self.num_players = tk.StringVar(self.setup_frame,"")
+        self.current = 1
         self.player_name = tk.StringVar(self.setup_frame,"")
-        self.player_name_2 = tk.StringVar(self.setup_frame, "")
-        self.player_name_3 = tk.StringVar(self.setup_frame, "")
-        self.player_name_4 = tk.StringVar(self.setup_frame, "")
-        self.player_name_5 = tk.StringVar(self.setup_frame, "")
-        self.player_name_6 = tk.StringVar(self.setup_frame, "")
-        self.player_name_7 = tk.StringVar(self.setup_frame, "")
-        self.player_name_8 = tk.StringVar(self.setup_frame, "")
-
+        self.chip_count = ""
 
         parent.rowconfigure(0,weight = 1)
         parent.columnconfigure(0, weight = 1)
 
         self.bg_label = tk.Label(self.setup_frame, relief=tk.SUNKEN, bg="light grey")
-        self.bg_label.grid(row=1, column=10, sticky="news", columnspan=10, rowspan=20)
+        self.bg_label.grid(row=0, column=10, sticky="news", columnspan=10, rowspan=20)
 
         self.num_players_label = tk.Label(self.setup_frame, text="Num of players:")
-        self.num_players_label.grid(row=2, column=2, columnspan=2)
+        self.num_players_label.grid(row=1, column=2, columnspan=2)
         self.num_players_entry = tk.Entry(self.setup_frame, textvariable = self.num_players)
-        self.num_players_entry.grid(row=2, column=5, columnspan=2)
+        self.num_players_entry.grid(row=1, column=5, columnspan=2)
         self.num_players_confirm = tk.Button(self.setup_frame, text="confirm", command=self.num_players_setup)
-        self.num_players_confirm.grid(row=2, column=7)
+        self.num_players_confirm.grid(row=1, column=7)
 
-        for i in range(1, 20):
+        self.chip_count_label = tk.Label(self.setup_frame,text = "Chip count")
+        self.chip_count_label.grid(row=5, column=2, columnspan=2)
+        self.chip_count_entry = tk.Entry(self.setup_frame,textvariable = self.chip_count)
+        self.chip_count_entry.grid(row=5, column=5, columnspan=2)
+        self.chip_count_button = tk.Button(self.setup_frame,text = "confirm",command = self.add_chip_counts)
+        self.chip_count_button.grid(row=5, column=7)
+
+
+
+        for i in range(0, 20):
             self.setup_frame.rowconfigure(i, weight=1)
             self.setup_frame.columnconfigure(i, weight=1)
 
     def num_players_setup(self):
-        num = int(self.num_players_entry.get())
+        self.player_name = ""
+        player_name_entry = tk.Entry(self.setup_frame, textvariable=self.player_name)
+        player_name_entry.grid(row=3, column=5, columnspan=2)
+        player_name_confirm = tk.Button(self.setup_frame, text="confirm",command=ft.partial(self.player_name_fn, self.current, player_name_entry))
+        player_name_confirm.grid(row=3, column=7)
 
-        for i in range(1, num + 1):
+    def player_name_fn(self, i,entry):
+        print(f"You pressed button {i} and the name was {entry.get()}")
+        name = entry.get()
+        if i % 2 == 0:
+            column = 15
+        else:
+            column = 10
 
-            player_label = tk.Label(self.setup_frame, relief=tk.RAISED, bg="light blue")
-            player_name = tk.Label(self.setup_frame, text=self.player_name.get())
+        if i <= 2:
+            row = 5
+        elif i <= 4:
+            row = 9
+        elif i <= 6:
+            row = 13
+        elif i <= 8:
+            row = 17
 
-            player_name_label = tk.Label(self.setup_frame, text=f"Player {i} name:")
-            player_name_label.grid(row=2 + i, column=2, columnspan=2)
-            player_name_entry = tk.Entry(self.setup_frame, textvariable=self.player_name)
-            player_name_entry.grid(row=2 + i, column=5, columnspan=2)
-            player_name_confirm = tk.Button(self.setup_frame, text="confirm", command=ft.partial(self.player_name_fn,i, player_name_entry))
-            player_name_confirm.grid(row=2 + i, column=7)
+        player_label = tk.Label(self.setup_frame, relief=tk.RAISED, bg="light blue")
+        player_label.grid(row=row, column=column, sticky="news", columnspan=5, rowspan=4)
+        player_name = tk.Label(self.setup_frame, text=name)
+        player_name.grid(row=row, column=column, rowspan=2)
 
+        self.current += 1
+
+        if self.current <= int(self.num_players.get()):
+            self.num_players_setup()
+
+
+    def add_chip_counts(self):
+        for i in range(self.current):
             if i % 2 == 0:
                 column = 15
             else:
                 column = 10
 
             if i <= 2:
-                row = 5
+                row = 6
             elif i <= 4:
-                row = 8
+                row = 10
             elif i <= 6:
-                row = 12
+                row = 14
             elif i <= 8:
-                row = 16
+                row = 18
 
-            player_label.grid(row=row, column=column, sticky="news", columnspan=5, rowspan=4)
-            player_name.grid(row=row, column=column, rowspan=2)
+            chip_label = tk.Label(self.setup_frame, text=self.chip_count)
+            chip_label.grid(row=row, column=column, rowspan=2)
 
-    def player_name_fn(self, i,entry_box):
-        print(f"You pressed button {i} and the name was {entry_box.get()}")
 
 
 
